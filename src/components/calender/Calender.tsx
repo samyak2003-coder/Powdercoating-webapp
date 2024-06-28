71958ae2a5577619086635350330524cf721aebf
samyak2003-coder <samyaksinghal2003@gmail.com>
2024-06-28 11:57:11 +0530

    Calender logging changed

--- src/components/calender/Calender.tsx
+++ src/components/calender/Calender.tsx
@@ -1,18 +1,37 @@
-import Calendar from 'react-calendar';
-import { useState } from 'react';
-import "./Calender.css"
+"use client"
+import React, { useState, useEffect } from 'react';
+import Calendar, { CalendarProps } from 'react-calendar';
+import './Calender.css'; 
 
 type ValuePiece = Date | null;
-type Value = ValuePiece | [ValuePiece, ValuePiece];
+type Value = ValuePiece;
 
-const Calender= () => {
-    const [value, onChange] = useState<Value>(new Date());
-    console.log(value);
-    return(
-        <>
-            <Calendar onChange={onChange} value={value} />
-        </>
-    )
+const Calender = () => {
+    const [value, setValue] = useState<Value>(new Date());
+
+    //to log the current date by default, value dependency automatically changes itself using useEffect
+    useEffect(() => {
+        if (value instanceof Date) {
+            const day = value.getDate();
+            const month = value.getMonth() + 1; // getMonth() is zero-based
+            const year = value.getFullYear();
+            console.log(`Day: ${day}, Month: ${month}, Year: ${year}`);
+        }
+    }, [value]);
+
+    //to log the date when user clicks on any date
+    const handleDateChange: CalendarProps['onChange'] = (date) => {
+        if (date instanceof Date) {
+            setValue(date);
+            const day = date.getDate();
+            const month = date.getMonth() + 1;
+            const year = date.getFullYear();
+        }
+    };
+
+    return (
+        <Calendar onChange={handleDateChange} value={value} />
+    );
 }
 
-export default Calender
+export default Calender;
--- src/components/container/Container.tsx
+++ src/components/container/Container.tsx
@@ -2,7 +2,10 @@
 import React, { useState } from 'react';
 import styles from './container.module.css';
 import Calender from "../calender/Calender"
+import Graph from "../graph/Graph"
 import { SlCalender } from "react-icons/sl";
+import { VscGraph } from "react-icons/vsc";
+import { FaTableList } from "react-icons/fa6";
 
 const Container: React.FC = () => {
   const [isCalenderVisible, setIsCalenderVisible] = useState(false);
@@ -29,6 +32,12 @@ const Container: React.FC = () => {
         <div className={`${styles.calender} ${isCalenderVisible ? styles.show : ''}`}>
           <Calender />
         </div>
+        <Graph/>
+        <div className="flex flex-row gap-0.5">
+        <VscGraph className={`${styles.graphButton}`}/>
+        <FaTableList className={`${styles.graphButton}`}/>
+        </div>
+ 
       </div>
     </>
   );
